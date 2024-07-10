72b49577b47fba2c909a4690367c7a44e436c1a5
Pranav Jerry <libreinator@disroot.org>
2024-07-10 12:58:44 +0530

    add configuration
    Remove secrets from the files.

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+
+// add your info here
 const firebaseConfig = {
-    apiKey: "AIzaSyDKHA6ZmllEUfjqmQLd0h5ujiPo0bObTc0",
-    authDomain: "inconnect-fb444.firebaseapp.com",
-    projectId: "inconnect-fb444",
-    storageBucket: "inconnect-fb444.appspot.com",
-    messagingSenderId: "114710194072",
-    appId: "1:114710194072:web:ee8de5852a8995789f2ded"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
